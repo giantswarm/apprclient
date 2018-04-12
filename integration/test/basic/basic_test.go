9c2da7047bdcc557ed3a110623b35223b3ff16c4
Federico Gimenez <federico@giantswarm.io>
2018-04-12 18:49:35 +0200

    add push tarball and promote methods

--- integration/test/basic/basic_test.go
+++ integration/test/basic/basic_test.go
@@ -3,8 +3,10 @@
 package basic
 
 import (
+	"strconv"
 	"testing"
 
+	"github.com/giantswarm/k8sportforward"
 	"github.com/giantswarm/micrologger"
 	"github.com/spf13/afero"
 
@@ -14,29 +16,63 @@ import (
 func Test_Client_GetReleaseVersion(t *testing.T) {
 	l, err := micrologger.New(micrologger.Config{})
 	if err != nil {
-		t.Errorf("could not create logger %v", err)
+		t.Fatalf("could not create logger %v", err)
+	}
+
+	restCfg := f.RestConfig()
+	fwc := k8sportforward.Config{
+		RestConfig: restCfg,
+	}
+
+	fw, err := k8sportforward.New(fwc)
+	if err != nil {
+		t.Fatalf("could not create forwarder %v", err)
+	}
+
+	podName, err := f.GetPodName("default", "app=cnr-server")
+	if err != nil {
+		t.Fatalf("could not get cnr-server pod name %v", err)
+	}
+	tc := k8sportforward.TunnelConfig{
+		Remote:    5000,
+		Namespace: "default",
+		PodName:   podName,
+	}
+	tunnel, err := fw.ForwardPort(tc)
+	if err != nil {
+		t.Fatalf("could not create tunnel %v", err)
 	}
 
 	c := apprclient.Config{
 		Fs:     afero.NewOsFs(),
 		Logger: l,
 
-		Address:      "http://localhost:5000",
+		Address:      "http://localhost:" + strconv.Itoa(tunnel.Local),
 		Organization: "giantswarm",
 	}
 
 	a, err := apprclient.New(c)
 	if err != nil {
-		t.Errorf("could not create appr %v", err)
+		t.Fatalf("could not create appr %v", err)
 	}
 
-	expected := "3.2.1"
-	actual, err := a.GetReleaseVersion("test-chart", "3-2-beta")
+	err = a.PushChartTarball("tb-chart", "5.5.5", "/e2e/fixtures/tb-chart.tar.gz")
 	if err != nil {
-		t.Errorf("could not get release %v", err)
+		t.Fatalf("could not push tarball %v", err)
+	}
+
+	err = a.PromoteChart("tb-chart", "5.5.5", "5-5-beta")
+	if err != nil {
+		t.Fatalf("could not promote chart %v", err)
+	}
+
+	expected := "5.5.5"
+	actual, err := a.GetReleaseVersion("tb-chart", "5-5-beta")
+	if err != nil {
+		t.Fatalf("could not get release %v", err)
 	}
 
 	if expected != actual {
-		t.Errorf("release didn't match expected, want %q, got %q", expected, actual)
+		t.Fatalf("release didn't match expected, want %q, got %q", expected, actual)
 	}
 }
