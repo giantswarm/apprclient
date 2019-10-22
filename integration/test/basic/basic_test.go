2f76bb5782052d7812786b664bee280b9971bf7f
Ross Fairbanks <rossf7@users.noreply.github.com>
2019-10-22 10:14:35 +0200

    Migrate e2e tests to KIND (#28)
    * vendor: updated deps
    
    * Remove unused gitignore
    
    * Remove e2e-harness from test
    
    * Use KIND in circle config
    
    * Add missing code
    
    * Whitespace
    
    * vendor: updated deps

--- integration/test/basic/basic_test.go
+++ integration/test/basic/basic_test.go
@@ -10,29 +10,21 @@ import (
 	"testing"
 	"time"
 
-	"github.com/cenkalti/backoff"
 	"github.com/giantswarm/apprclient"
-	"github.com/giantswarm/e2e-harness/pkg/framework"
+	"github.com/giantswarm/backoff"
 	"github.com/giantswarm/k8sportforward"
 	"github.com/giantswarm/microerror"
-	"github.com/giantswarm/micrologger"
 	"github.com/spf13/afero"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 func testSetup(ctx context.Context, t *testing.T) (*apprclient.Client, *k8sportforward.Tunnel) {
 	var err error
 
-	l, err := micrologger.New(micrologger.Config{})
-	if err != nil {
-		t.Fatalf("could not create logger %v", err)
-	}
-
 	var fw *k8sportforward.Forwarder
 	{
-		restCfg := h.RestConfig()
-
 		c := k8sportforward.ForwarderConfig{
-			RestConfig: restCfg,
+			RestConfig: config.CPK8sClients.RESTConfig(),
 		}
 
 		fw, err = k8sportforward.NewForwarder(c)
@@ -41,24 +33,31 @@ func testSetup(ctx context.Context, t *testing.T) (*apprclient.Client, *k8sportf
 		}
 	}
 
-	podName, err := h.GetPodName("default", "app=cnr-server")
-	if err != nil {
-		t.Fatalf("could not get cnr-server pod name %v", err)
+	var podName string
+	{
+		podName, err = waitForPod()
+		if err != nil {
+			t.Fatalf("could not get cnr-server pod %#v", err)
+		}
 	}
-	tunnel, err := fw.ForwardPort("default", podName, 5000)
-	if err != nil {
-		t.Fatalf("could not create tunnel %v", err)
+
+	var tunnel *k8sportforward.Tunnel
+	{
+		tunnel, err = fw.ForwardPort("default", podName, 5000)
+		if err != nil {
+			t.Fatalf("could not create tunnel %v", err)
+		}
 	}
 
 	serverAddress := "http://" + tunnel.LocalAddress()
-	err = waitForServer(h, serverAddress+"/cnr/api/v1/packages")
+	err = waitForServer(serverAddress + "/cnr/api/v1/packages")
 	if err != nil {
 		t.Fatalf("server didn't come up on time")
 	}
 
 	c := apprclient.Config{
 		Fs:     afero.NewOsFs(),
-		Logger: l,
+		Logger: config.Logger,
 
 		Address:      serverAddress,
 		Organization: "giantswarm",
@@ -68,6 +67,7 @@ func testSetup(ctx context.Context, t *testing.T) (*apprclient.Client, *k8sportf
 	if err != nil {
 		t.Fatalf("could not create appr %v", err)
 	}
+
 	return a, tunnel
 }
 
@@ -81,9 +81,8 @@ func testTeardown(ctx context.Context, a *apprclient.Client, tunnel *k8sportforw
 }
 
 func Test_Client_GetReleaseVersion(t *testing.T) {
-	var err error
-
 	ctx := context.Background()
+	var err error
 
 	a, tunnel := testSetup(ctx, t)
 	defer testTeardown(ctx, a, tunnel, t)
@@ -109,7 +108,41 @@ func Test_Client_GetReleaseVersion(t *testing.T) {
 	}
 }
 
-func waitForServer(h *framework.Host, url string) error {
+func waitForPod() (string, error) {
+	var podName string
+
+	o := func() error {
+		lo := metav1.ListOptions{
+			FieldSelector: "status.phase=Running",
+			LabelSelector: "app=cnr-server",
+		}
+		pods, err := config.CPK8sClients.K8sClient().CoreV1().Pods(metav1.NamespaceDefault).List(lo)
+		if err != nil {
+			return microerror.Mask(err)
+		}
+		if len(pods.Items) != 1 {
+			return microerror.Maskf(executionFailedError, "expected 1 pod got %d", len(pods.Items))
+		}
+
+		pod := pods.Items[0]
+		podName = pod.Name
+
+		return nil
+	}
+
+	n := func(err error, t time.Duration) {
+		log.Printf("waiting for server at %s: %#v", t, err)
+	}
+
+	err := backoff.RetryNotify(o, backoff.NewConstant(2*time.Minute, 15*time.Second), n)
+	if err != nil {
+		return "", microerror.Mask(err)
+	}
+
+	return podName, nil
+}
+
+func waitForServer(url string) error {
 	var err error
 
 	operation := func() error {
@@ -124,7 +157,7 @@ func waitForServer(h *framework.Host, url string) error {
 		log.Printf("waiting for server at %s: %v", t, err)
 	}
 
-	err = backoff.RetryNotify(operation, backoff.NewExponentialBackOff(), notify)
+	err = backoff.RetryNotify(operation, backoff.NewExponential(2*time.Minute, 30*time.Second), notify)
 	if err != nil {
 		return microerror.Mask(err)
 	}
